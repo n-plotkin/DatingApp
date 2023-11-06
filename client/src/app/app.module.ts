b2e4fa13e7cd63f780a465dafb99deabc85d8159
n-plotkin <nplotkin@oberlin.edu>
2023-11-06 12:07:24 -0500

    Use memberservice to store state and avoid redundant API calls.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -23,6 +23,7 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -54,6 +55,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
     //this serves as middleware
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
     {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true},
 
   ],
   bootstrap: [AppComponent]
